f620bfc26be79c80b5fb9978cd0349c827c07b05
Noa Second <contact@noasecond.com>
2024-05-13 11:42:06 +0200

    :sparkles: Add contact form and update texts

--- contact.php
+++ contact.php
@@ -10,13 +10,30 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'en';
 <head>
     <title><?php getValueFromJson('title'); ?></title>
     <?php include 'components/head.php'; ?>
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/responsive/contact.css">
 </head>
 
 <body>
     <header>
         <?php include ('components/header.php'); ?>
     </header>
-    <main></main>
+    <main>
+        <h1><?php getValueFromJson('description'); ?></h1>
+        <form action="components/mail.php" method="post">
+            <div id="formRight">
+                <div>
+                    <input type="text" id="firstname" name="firstname" placeholder="<?php getValueFromJson('form.firstname'); ?>" required>
+                    <input type="text" id="lastname" name="lastname" placeholder="<?php getValueFromJson('form.lastname'); ?>" required>
+                </div>
+                <input type="email" id="email" name="email" placeholder="<?php getValueFromJson('form.email'); ?>" required>
+                <input type="submit" value="<?php getValueFromJson('form.submit'); ?>">
+            </div>
+            <div id="formLeft">
+                <input type="text" id="subject" name="subject" placeholder="<?php getValueFromJson('form.subject'); ?>" required>
+                <textarea id="message" name="message" placeholder="<?php getValueFromJson('form.message'); ?>" required></textarea>
+            </div>
+    </main>
     <footer>
         <?php include ('components/footer.php'); ?>
     </footer>
