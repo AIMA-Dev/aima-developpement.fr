803a5c375d683a81b8ba17ea96c189004973f232
Noa Second <contact@noasecond.com>
2024-05-14 11:39:55 +0200

    :zap: Improve performances and structure

--- utils/generate-sitemap.js
+++ utils/generate-sitemap.js
@@ -5,7 +5,7 @@ const path = require('path');
 function generateSitemap(pages) {
     const baseUrl = 'http://localhost:3000';
     const currentDate = new Date().toISOString().split('T')[0];
-    
+
     let xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
 
@@ -26,13 +26,10 @@ function generateSitemap(pages) {
 }
 
 // List pages to include in the sitemap
-const pages = [
-    '/',
-    '/cyclotrons',
-    '/contact',
-    '/teams'
-];
-
+const excludedPages = ['admin.php', '404.php', '500.php', '403.php'];
+const pages = fs.readdirSync(path.join(__dirname, '../'))
+    .filter(file => file.endsWith('.php') && !excludedPages.includes(file))
+    .map(file => `/${file.replace('.php', '')}`);
 
 // Write content to sitemap.xml
 const sitemapPath = path.join(__dirname, 'sitemap.xml');
