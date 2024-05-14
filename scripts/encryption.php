422600e316546a63863ee1af8e1b0d621e7940bf
Noa Second <contact@noasecond.com>
2024-05-14 13:34:18 +0200

    :lock: Add encryption algs

---
+++ scripts/encryption.php
@@ -0,0 +1,24 @@
+<?php
+function encrypt($string)
+{
+    $key = '###'; // Secret
+    $cipher = 'AES-256-CBC';
+    $ivLength = openssl_cipher_iv_length($cipher);
+    $iv = openssl_random_pseudo_bytes($ivLength);
+    $encrypted = openssl_encrypt($string, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+    $encryptedString = base64_encode($iv . $encrypted);
+    return $encryptedString;
+}
+
+function decrypt($encryptedString)
+{
+    $key = '###'; // Secret
+    $cipher = 'AES-256-CBC';
+    $ivLength = openssl_cipher_iv_length($cipher);
+    $data = base64_decode($encryptedString);
+    $iv = substr($data, 0, $ivLength);
+    $encrypted = substr($data, $ivLength);
+    $decrypted = openssl_decrypt($encrypted, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+    return $decrypted;
+}
+?>
