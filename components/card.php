74ffd4970134affb89ef80dd4cc1ce9b7deea903
Noa Second <contact@noasecond.com>
2024-05-15 12:11:12 +0200

    :lipstick: Enhance global style and components

--- components/card.php
+++ components/card.php
@@ -1,5 +1,5 @@
 <div class="card" onclick="window.location.href = '<?php echo $link ?>';">
-    <img src="<?php echo $imgUrl; ?>"
+    <img src="<?php echo ($imgUrl !== '') ? $imgUrl : 'https://placehold.co/500x500'; ?>"
         alt="<?php echo $title; ?>" style="width:100%">
     <div class="cardContainer">
         <h4><b><?php echo $title; ?></b></h4>
