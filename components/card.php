f65dc4626c3757b51393d5f0d0f00b10083293cc
Noa Second <contact@noasecond.com>
2024-05-15 11:08:59 +0200

    :sparkles: Add new content

---
+++ components/card.php
@@ -0,0 +1,8 @@
+<div class="card" onclick="window.location.href = '<?php echo $link ?>';">
+    <img src="<?php echo $imgUrl; ?>"
+        alt="<?php echo $title; ?>" style="width:100%">
+    <div class="cardContainer">
+        <h4><b><?php echo $title; ?></b></h4>
+        <p><?php echo $job; ?></p>
+    </div>
+</div>
--- team.php
+++ team.php
@@ -10,13 +10,47 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'en';
 <head>
     <title><?php getValueFromJson('title'); ?></title>
     <?php include 'components/head.php'; ?>
+    <link rel="stylesheet" href="css/team.css">
+    <link rel="stylesheet" href="css/responsive/team.css">
 </head>
 
 <body>
     <header>
         <?php include ('components/header.php'); ?>
     </header>
-    <main></main>
+    <main>
+        <h1><?php getValueFromJson('description'); ?></h1>
+        <div id="classWrap">
+            <?php
+            $title = getValueFromJson('pierremandrillion.name', false);
+            $job = getValueFromJson('pierremandrillion.job', false);
+            $imgUrl = getValueFromJson('pierremandrillion.image', false);
+            $link = getValueFromJson('pierremandrillion.linkedin', false);
+            include 'components/card.php';
+            ?>
+            <?php
+            $title = getValueFromJson('matthieuconjat.name', false);
+            $job = getValueFromJson('matthieuconjat.job', false);
+            $imgUrl = getValueFromJson('matthieuconjat.image', false);
+            $link = getValueFromJson('matthieuconjat.linkedin', false);
+            include 'components/card.php';
+            ?>
+            <?php
+            $title = getValueFromJson('raphaëldevianne.name', false);
+            $job = getValueFromJson('raphaëldevianne.job', false);
+            $imgUrl = getValueFromJson('raphaëldevianne.image', false);
+            $link = getValueFromJson('raphaëldevianne.linkedin', false);
+            include 'components/card.php';
+            ?>
+            <?php
+            $title = getValueFromJson('antoinezoude.name', false);
+            $job = getValueFromJson('antoinezoude.job', false);
+            $imgUrl = getValueFromJson('antoinezoude.image', false);
+            $link = getValueFromJson('antoinezoude.linkedin', false);
+            include 'components/card.php';
+            ?>
+        </div>
+    </main>
     <footer>
         <?php include ('components/footer.php'); ?>
     </footer>
