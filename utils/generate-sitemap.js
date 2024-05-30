e9654091d67a8230707e7e28de8d906268e66b38
Noa Second <contact@noasecond.com>
2024-05-30 11:45:59 +0200

    :bug: Fix js commentary

--- utils/generate-sitemap.js
+++ utils/generate-sitemap.js
@@ -46,5 +46,3 @@ fs.writeFile(sitemapPath, sitemapContent, err => {
         console.log('The file sitemap.xml has been generated successfully!');
     }
 });
-
-# Développé avec ❤️ par : www.noasecond.com.
