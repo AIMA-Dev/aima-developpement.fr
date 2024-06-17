eeca620c151df34d8842842ad66464d12bebca01
Noa Second <contact@noasecond.com>
2024-06-17 10:35:58 +0200

    :sparkles: Update Team content

--- team.php
+++ team.php
@@ -29,28 +29,28 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $deviceLang;
             $title = getValueFromJson('pierremandrillion.name', false);
             $job = getValueFromJson('pierremandrillion.job', false);
             $imgUrl = getValueFromJson('pierremandrillion.image', false);
-            $link = getValueFromJson('pierremandrillion.linkedin', false);
+            $link = getValueFromJson('pierremandrillion.url', false);
             include 'components/card.php';
             ?>
             <?php
             $title = getValueFromJson('matthieuconjat.name', false);
             $job = getValueFromJson('matthieuconjat.job', false);
             $imgUrl = getValueFromJson('matthieuconjat.image', false);
-            $link = getValueFromJson('matthieuconjat.linkedin', false);
+            $link = getValueFromJson('matthieuconjat.url', false);
             include 'components/card.php';
             ?>
             <?php
             $title = getValueFromJson('raphaëldevianne.name', false);
             $job = getValueFromJson('raphaëldevianne.job', false);
             $imgUrl = getValueFromJson('raphaëldevianne.image', false);
-            $link = getValueFromJson('raphaëldevianne.linkedin', false);
+            $link = getValueFromJson('raphaëldevianne.url', false);
             include 'components/card.php';
             ?>
             <?php
             $title = getValueFromJson('antoinezoude.name', false);
             $job = getValueFromJson('antoinezoude.job', false);
             $imgUrl = getValueFromJson('antoinezoude.image', false);
-            $link = getValueFromJson('antoinezoude.linkedin', false);
+            $link = getValueFromJson('antoinezoude.url', false);
             include 'components/card.php';
             ?>
         </div>
@@ -58,76 +58,83 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $deviceLang;
         <h3><?php getValueFromJson('scientificadvisory'); ?></h3>
         <div class="classWrap">
             <?php
-            $title = "Prof. Georfes Charpak (1924 - 2010)";
-            $job = "Prix Nobel de Physique 1992";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('georgescharpak.name', false);
+            $job = getValueFromJson('georgescharpak.job', false);
+            $imgUrl = getValueFromJson('georgescharpak.image', false);
+            $link = getValueFromJson('georgescharpak.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Prof. François Demard";
-            $job = "Directeur honoraire centre Antoine Lacassagne";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('francoisdemard.name', false);
+            $job = getValueFromJson('francoisdemard.job', false);
+            $imgUrl = getValueFromJson('francoisdemard.image', false);
+            $link = getValueFromJson('francoisdemard.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Dr. Renaud Guignard";
-            $job = "Médecin nucléariste";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('renaudguignard.name', false);
+            $job = getValueFromJson('renaudguignard.job', false);
+            $imgUrl = getValueFromJson('renaudguignard.image', false);
+            $link = getValueFromJson('renaudguignard.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Gilles Viot";
-            $job = "Expert radiochimiste";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('gillesviot.name', false);
+            $job = getValueFromJson('gillesviot.job', false);
+            $imgUrl = getValueFromJson('gillesviot.image', false);
+            $link = getValueFromJson('gillesviot.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Dr. Joel Hérault";
-            $job = "Expert en physique médicale";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('joelherault.name', false);
+            $job = getValueFromJson('joelherault.job', false);
+            $imgUrl = getValueFromJson('joelherault.image', false);
+            $link = getValueFromJson('joelherault.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Prof. Jérome Doyen";
-            $job = "Oncologue radiothérapeute";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('alessandralombardi.name', false);
+            $job = getValueFromJson('alessandralombardi.job', false);
+            $imgUrl = getValueFromJson('alessandralombardi.image', false);
+            $link = getValueFromJson('alessandralombardi.url', false);
+            include 'components/card.php';
+            ?>
+            <?php
+            $title = getValueFromJson('jeromedoyen.name', false);
+            $job = getValueFromJson('jeromedoyen.job', false);
+            $imgUrl = getValueFromJson('jeromedoyen.image', false);
+            $link = getValueFromJson('jeromedoyen.url', false);
             include 'components/card.php';
             ?>
         </div>
         <h3><?php getValueFromJson('technicalboard'); ?></h3>
         <div class="classWrap">
             <?php
-            $title = "Jean-Claude Grini";
-            $job = "Expert électronicien";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('jeanclaudegrini.name', false);
+            $job = getValueFromJson('jeanclaudegrini.job', false);
+            $imgUrl = getValueFromJson('jeanclaudegrini.image', false);
+            $link = getValueFromJson('jeanclaudegrini.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Guillaume Magrin";
-            $job = "Expert accel. médicaux";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('guillaumemagrin.name', false);
+            $job = getValueFromJson('guillaumemagrin.job', false);
+            $imgUrl = getValueFromJson('guillaumemagrin.image', false);
+            $link = getValueFromJson('guillaumemagrin.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Henri Braquet";
-            $job = "Expert comptabilité";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('henribraquet.name', false);
+            $job = getValueFromJson('henribraquet.job', false);
+            $imgUrl = getValueFromJson('henribraquet.image', false);
+            $link = getValueFromJson('henribraquet.url', false);
             include 'components/card.php';
             ?>
             <?php
-            $title = "Alexandre Giusto";
-            $job = "Ex chef BE Medicyc";
-            $imgUrl = "";
-            $link = "";
+            $title = getValueFromJson('alexandregiusto.name', false);
+            $job = getValueFromJson('alexandregiusto.job', false);
+            $imgUrl = getValueFromJson('alexandregiusto.image', false);
+            $link = getValueFromJson('alexandregiusto.url', false);
             include 'components/card.php';
             ?>
         </div>
