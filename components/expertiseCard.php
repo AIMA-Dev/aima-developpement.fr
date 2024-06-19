aa218364013e67f7c1398f01545ae9fd8bc78b25
Noa Second <contact@noasecond.com>
2024-06-19 11:11:41 +0200

    :sparkles: Add expertise cards into the home page

---
+++ components/expertiseCard.php
@@ -0,0 +1,29 @@
+<div class="expertiseCard">
+    <div class="colorBlock left"<?php if ($side === "right") echo 'style="background-color: transparent;"'; ?>></div>
+    <div class="centerContent">
+        <h2 <?php if ($side === "left") echo 'style="text-align: end;"'; ?>><?php echo $title;?></h2>
+        <div <?php if ($side === "left") echo 'style="flex-direction: row-reverse;"'; ?>>
+            <div class="imgWrap" <?php if ($side === "left") echo 'style="margin-right: 0px; margin-left: 20px;"'; ?>>
+                <?php
+                    foreach ($images as $image) {
+                        if ($image === $images[0]) {
+                            echo '<img src="' . $image . '" alt="" class="imgActive">';
+                        } else {
+                            echo '<img src="' . $image . '" alt="" class="imgInactive">';
+                        }
+                    }
+                ?>
+                <div class="dotWrap">
+                    <?php
+                        foreach ($images as $index => $image) {
+                            $dotClass = ($index === 0) ? 'dotActive' : '';
+                            echo '<span class="dot ' . $dotClass . '" onclick="currentSlide(' . ($index + 1) . ')"></span>';
+                        }
+                    ?>
+                </div>
+            </div>
+            <a><?php echo $text;?></a>
+        </div>
+    </div>
+    <div class="colorBlock right"<?php if ($side === "left") echo 'style="background-color: transparent;"'; ?>></div>
+</div>
--- index.php
+++ index.php
@@ -20,6 +20,44 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $deviceLang;
         <?php include ('components/header.php'); ?>
     </header>
     <main>
+        <h1>Our story</h1>
+        <a>AIMA Développement is designing, building and testing new cost-effective cyclotrons and dedicated beam lines
+            for various medical and industrial applications since 2005. The expertise stems from the cyclotron
+            laboratory of the Centre Antoine Lacassagne (CAL), that designed and built MEDICYC, the first hospital-based
+            cyclotron facility in France for protontherapy. At this time, the MEDICYC project was led by Pierre
+            Mandrillon. Before retiring from the CAL and the MEDICYC project, he founded AIMA, and is still its CEO.</a>
+        <div>
+            <?php
+                $side = 'right';
+                $title = getValueFromJson('expertise.1.title', false);
+                $text = getValueFromJson('expertise.1.text', false);
+                $images = [
+                    'https://placehold.co/1x1',
+                    'https://placehold.co/2x2',
+                    'https://placehold.co/3x3'
+                ];
+                include ('components/expertiseCard.php');
+            ?>
+            <?php
+                $side = 'left';
+                $title = getValueFromJson('expertise.2.title', false);
+                $text = getValueFromJson('expertise.2.text', false);
+                $images = [
+                    'https://placehold.co/1x1'
+                ];
+                include ('components/expertiseCard.php');
+            ?>
+            <?php
+                $side = 'right';
+                $title = getValueFromJson('expertise.3.title', false);
+                $text = getValueFromJson('expertise.3.text', false);
+                $images = [
+                    'https://placehold.co/1x1',
+                    'https://placehold.co/2x2'
+                ];
+                include ('components/expertiseCard.php');
+            ?>
+        </div>
     </main>
     <footer>
         <?php include ('components/footer.php'); ?>
