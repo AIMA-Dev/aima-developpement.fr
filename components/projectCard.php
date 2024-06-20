c2e211692ce0e4821fdf37f2b5845e3b75231641
Noa Second <contact@noasecond.com>
2024-06-20 14:49:31 +0200

    :sparkles: Add project cards

---
+++ components/projectCard.php
@@ -0,0 +1,8 @@
+<div class="projectCard">
+    <img src="<?php echo $image ?>" alt="cyclotron">
+    <div class="content">
+        <a><?php echo $title ?></a><br>
+        <a><?php echo $description ?></a><br>
+        <a href="#"><?php echo $more ?><img src="assets/img/icons/arrow.svg" alt="arrow"></a>
+    </div>
+</div>
--- projects.php
+++ projects.php
@@ -14,6 +14,7 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $deviceLang;
     <title><?php getValueFromJson('title'); ?></title>
     <?php include 'components/head.php'; ?>
     <link rel="stylesheet" href="css/projects.css">
+    <link rel="stylesheet" href="css/projectCard.css">
 </head>
 
 <body>
@@ -21,7 +22,42 @@ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : $deviceLang;
         <?php include ('components/header.php'); ?>
     </header>
     <main>
-        <h1><?php getValueFromJson('description'); ?></h1>
+        <h1><?php getValueFromJson('cyclotrons.title'); ?></h1>
+        <section id="cyclotronsWrap">
+            <?php
+            $image = "https://placehold.co/600x400/orange/white";
+            $title = getValueFromJson('cyclotrons.1.title', false);
+            $description = getValueFromJson('cyclotrons.1.description', false);
+            $stat = getValueFromJson('cyclotrons.1.stat', false);
+            $more = getValueFromJson('more', false);
+            include ('components/projectCard.php');
+            ?>
+            <?php
+            $image = "https://placehold.co/600x400/orange/white";
+            $title = getValueFromJson('cyclotrons.2.title', false);
+            $description = getValueFromJson('cyclotrons.2.description', false);
+            $stat = getValueFromJson('cyclotrons.2.stat', false);
+            $more = getValueFromJson('more', false);
+            include ('components/projectCard.php');
+            ?>
+            <?php
+            $image = "https://placehold.co/600x400/orange/white";
+            $title = getValueFromJson('cyclotrons.3.title', false);
+            $description = getValueFromJson('cyclotrons.3.description', false);
+            $stat = getValueFromJson('cyclotrons.3.stat', false);
+            $more = getValueFromJson('more', false);
+            include ('components/projectCard.php');
+            ?>
+        </section>
+        <h1><?php getValueFromJson('nuclear.title'); ?></h1>
+        <?php
+        $image = "https://placehold.co/600x400/orange/white";
+        $title = getValueFromJson('nuclear.1.title', false);
+        $description = getValueFromJson('nuclear.1.description', false);
+        $stat = getValueFromJson('nuclear.1.stat', false);
+        $more = getValueFromJson('more', false);
+        include ('components/projectCard.php');
+        ?>
     </main>
     <footer>
         <?php include ('components/footer.php'); ?>
