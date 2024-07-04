e5feb2e2a6ad74d9cbf68469ae73775766db2bc8
Noa Second <contact@noasecond.com>
2024-07-04 09:51:23 +0200

    :mag: Improve SEO

--- components/head.php
+++ components/head.php
@@ -28,23 +28,30 @@ if (in_array($_SERVER['PHP_SELF'], $pagesAffectedByMaintenance)) {
 <!-- Global Meta Tags -->
 <meta charset="UTF-8">
 <meta http-equiv="Cache-control" content="public">
-<meta name="robots" content="follow">
+<meta name="robots" content="index, follow">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="theme-color" content="#5E031F" />
 <meta name="theme-color" media="(prefers-color-scheme: light)" content="#5E031F" />
 <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#5E031F" />
 <meta name="description" content="Accélérateurs de particules pour les applications industrielles et médicales" />
-<!-- OpenGraph / Facebook -->
+<meta name="keywords" content="AIMA, Developpement, AIMA Developpement, Cyclotrons, medical, industrial">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="revisit-after" content="1 days">
+<meta name="author" content="Noa Second">
+<!-- OpenGraph -->
 <meta property="og:title" content="AIMA Developpement" />
 <meta property="og:description" content="Accelerator for Industrial and Medical Applications" />
 <meta property="og:image" content="http://www.aima-developpement.fr/assets/img/logo_3200x2400 - White - Glyphe.webp" />
 <meta property="og:image:alt" content="AIMA Developpement Logo" />
+<!-- Google -->
+<meta itemprop="name" content="AIMA Developpement">
+<meta itemprop="description" content="Accelerator for Industrial and Medical Applications">
+<meta itemprop="image" content="http://www.aima-developpement.fr/assets/img/logo_3200x2400 - White - Glyphe.webp">
 <!-- Twitter -->
 <meta name="twitter:title" content="AIMA Developpement" />
 <meta name="twitter:description" content="Accelerator for Industrial and Medical Applications" />
 <meta name="twitter:url" content="https://www.aima-developpement.fr/" />
-<meta name="twitter:image:src"
-    content="http://www.aima-developpement.fr/assets/img/logo_3200x2400 - White - Glyphe.webp" />
+<meta name="twitter:image:src" content="http://www.aima-developpement.fr/assets/img/logo_3200x2400 - White - Glyphe.webp" />
 <meta name="twitter:image:alt" content="AIMA Developpement Logo" />
 <!-- Favicon -->
 <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicon/apple-touch-icon.webp">
@@ -70,7 +77,6 @@ if (in_array($_SERVER['PHP_SELF'], $pagesAffectedByMaintenance)) {
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
-    rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
 <!-- © AIMA DEVELOPPEMENT 2024 -->
