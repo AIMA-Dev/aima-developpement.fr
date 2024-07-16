ce1127ac7c72e560427f5ca6b72603e3977bc2e4
GitHub Action <action@github.com>
2024-07-16 13:35:24 +0000

    :construction_worker: Add copyright to files

--- js/numericTools.js
+++ js/numericTools.js
@@ -2,3 +2,4 @@ const slideshowContainers = document.querySelectorAll('.slideshowContainer');
 slideshowContainers.forEach(container => {
     container.classList.add('show');
 });
+// © AIMA DEVELOPPEMENT 2024
